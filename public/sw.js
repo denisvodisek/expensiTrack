f6f5ea3eef9271bcbf2846f791d7a1e65cc5722e
denisvodiseksw <denis.vodisek@stepworks.co>
2025-11-13 19:45:09 +0800

    Update service worker version to v1.0.3

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Version-based cache name - update this with each deployment
-const CACHE_VERSION = 'v1.0.2';
+const CACHE_VERSION = 'v1.0.3';
 const CACHE_NAME = `expensitrak-${CACHE_VERSION}`;
 
 const urlsToCache = [
